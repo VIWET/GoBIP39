991b4d33ccf7bbbdddbe11049a4ba00d62558769
Dmitry Sidorov <viwet42@gmail.com>
2024-11-18 20:08:26 +0100

    feat(entropy): add entropy and checksum entorpy types
    - add checksum calculation method
    
    - add checksum validation method
    
    - add checksum removal method

--- entropy.go
+++ entropy.go
@@ -1,6 +1,11 @@
 package gobip39
 
-import "errors"
+import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
+	"math/big"
+)
 
 const (
 	// Minimum allowed by BIP-39 entropy bitlen
@@ -15,18 +20,103 @@ const (
 )
 
 // ErrInvalidEntropyBitlen
-var ErrInvalidEntropyBitlen = errors.New("entropy bitlen must be one of [128, 160, 192, 224, 256]")
+var ErrInvalidEntropyBitlen = errors.New("entropy bitlen must be one of 128, 160, 192, 224 or 256")
+
+type (
+	// Initial entropy
+	Entropy []byte
+	// Entropy with checksum
+	ChecksumEntropy []byte
+)
 
 // NewEntropy generates new random entropy of bitlen
-func NewEntropy(bitlen uint) ([]byte, error) {
+func NewEntropy(bitlen uint) (Entropy, error) {
 	if !isValidBitlen(bitlen) {
 		return nil, ErrInvalidEntropyBitlen
 	}
 
-	return RandomBytes(BitlenToBytelen(bitlen)), nil
+	return Entropy(RandomBytes(BitlenToBytelen(bitlen))), nil
 }
 
 // isValidBitlen returns false if the bitlen is less than 128, greater than 256, or not divisible by 32
 func isValidBitlen(bitlen uint) bool {
 	return bitlen >= MinBitlen && bitlen <= MaxBitlen && bitlen%BitlenDivisor == 0
+}
+
+// Bitlen return a bitlen of entropy
+func (e Entropy) Bitlen() uint {
+	return BytelenToBitlen(uint(len(e)))
+}
+
+// AddChecksum returns entropy with checksum
+func (e Entropy) AddChecksum() ChecksumEntropy {
+	var (
+		hash           = sha256.Sum256(e)
+		checksumBitlen = e.Bitlen() / BitlenDivisor
+		shift          = 256 - checksumBitlen
+	)
+
+	checksum := new(big.Int).Rsh(new(big.Int).SetBytes(hash[:]), shift)
+
+	return PadLeftToBitlen(
+		new(big.Int).Add(
+			new(big.Int).Lsh(
+				new(big.Int).SetBytes(e),
+				checksumBitlen,
+			),
+			checksum,
+		).Bytes(),
+		e.Bitlen()+checksumBitlen,
+	)
+}
+
+// IsValidChecksum validates entorpy checksum
+func (e Entropy) IsValidChecksum(checksum []byte) bool {
+	var (
+		hash           = sha256.Sum256(e)
+		checksumBitlen = e.Bitlen() / BitlenDivisor
+		shift          = 256 - checksumBitlen
+	)
+
+	return bytes.Equal(
+		new(big.Int).Rsh(
+			new(big.Int).SetBytes(hash[:]),
+			shift,
+		).Bytes(),
+		checksum,
+	)
+}
+
+// Bitlen returns a bitlen of entorpy with checksum
+func (e ChecksumEntropy) Bitlen() uint {
+	bitlen := BytelenToBitlen(uint(len(e)) - 1)
+	return bitlen + bitlen/BitlenDivisor
+}
+
+// EntropyBitlen returns a bitlen of entropy without checksum
+func (e ChecksumEntropy) EntropyBitlen() uint {
+	return BytelenToBitlen(uint(len(e)) - 1)
+}
+
+// RemoveChecksum returns initial entropy and checksum separately
+func (e ChecksumEntropy) RemoveChecksum() (Entropy, []byte) {
+	var (
+		bitlen         = e.Bitlen()
+		entropyBitlen  = e.EntropyBitlen()
+		checksumBitlen = bitlen - entropyBitlen
+	)
+
+	checksumEntropy := new(big.Int).SetBytes(e)
+
+	entropy := new(big.Int).Rsh(
+		checksumEntropy,
+		checksumBitlen,
+	).Bytes()
+
+	checksum := new(big.Int).And(
+		checksumEntropy,
+		new(big.Int).SetUint64(uint64(1<<checksumBitlen)-1),
+	).Bytes()
+
+	return PadLeftToBitlen(entropy, entropyBitlen), checksum
 }
